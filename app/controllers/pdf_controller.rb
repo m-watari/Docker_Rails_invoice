1dba24427eab00f571e55f46960e129348080b84
m-watari <watari141@gmail.com>
2022-01-21 23:28:54 +0900

    wickd_pdf文字化け解消

---
+++ app/controllers/pdf_controller.rb
@@ -0,0 +1,16 @@
+class PdfController < ApplicationController
+    
+    def download
+        @invoice = Invoice.find(1)
+            respond_to do |format|
+            # format.html
+            format.pdf do
+                render pdf: "pdf_download_test",   # PDFのファイル名
+                        encording: 'UTF-8',         # エンコード指定
+                        layout: 'layouts/pdf.html', # PDF用の共通レイアウト
+                        # trueを指定すると、HTML画面として確認ができる
+                        show_as_html: params[:show_as_html].present?
+            end
+        end
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :invoice_details
   resources :invoices
   resources :members
+  # PDF出力
+  get '/pdf-download', to: 'pdf#download', as: 'pdf_download', :format => 'pdf'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
