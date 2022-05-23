49ce1035b3c1388c5744cd9b080bab8b24346139
MamoruWatari <mwatari@obi.sg>
2022-05-23 22:30:34 +0900

    #group作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_22_000149) do
+ActiveRecord::Schema.define(version: 2022_05_23_123523) do
 
   create_table "group_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -22,7 +22,10 @@ ActiveRecord::Schema.define(version: 2022_05_22_000149) do
   end
 
   create_table "groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
+    t.text "introduction"
+    t.string "image_id"
+    t.integer "owner_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
